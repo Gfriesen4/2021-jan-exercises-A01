0e26901da823fae7fce44ce4139199075dbe5fac
Greg Friesen <59981609+Gfriesen4@users.noreply.github.com>
2021-01-14 14:35:41 -0700

    Set up DAL, BLL and controllers
    Set up DAL, BLL and controllers

---
+++ ChinookDemoSolution/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.Entities;
+using ChinookSystem.DAL;
+using ChinookSystem.VireModels;
+using System.ComponentModel; //is for ODS
+#endregion
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        //Due to the fact that the entities are Internal, You CAN NOT use the Entity class as a return Datatype
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ArtistAlbums> Albums_GetArtistAlbums()
+        {
+            using(var context = new ChinookSystemContext())
+            {
+                //Linq to Entity
+
+                IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    select new ArtistAlbums
+                                                    {
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistName = x.Artist.Name
+                                                    };
+                return results.ToList();
+            }   
+
+        }
+    }
+}
